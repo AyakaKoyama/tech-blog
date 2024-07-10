b9694b9ad0a168156ed6c9943489d3e4addc4266
AyakaKoyama <ayamail2000@gmail.com>
2024-07-11 08:00:14 +0900

    first-commit

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(ts|tsx|js|jsx)$/,
+      exclude: /node_modules/,
+    });
+    return config;
+  },
+};
