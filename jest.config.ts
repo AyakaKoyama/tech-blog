c177ec60583ed1bab8304027e916787252c17d47
AyakaKoyama <ayamail2000@gmail.com>
2024-08-03 15:56:41 +0900

    rewrite-jest

--- jest.config.ts
+++ jest.config.ts
@@ -6,18 +6,13 @@ const createJestConfig = nextJest({
 });
 
 const config: Config = {
-  //react-markdown を babel-jest でコンパイルして ESM => CJS に変更
-  // preset: "ts-jest/presets/js-with-babel",
-  // globals: {
-  //   "ts-jest": {
-  //     tsconfig: "tsconfig.json",
-  //     useESM: true,
-  //   },
-  // },
-  // transform: {
-  //   "^.+\\.(js|jsx|ts|tsx)$": "babel-jest", // Babelで変換
-  // },
-  // transformIgnorePatterns: ["/node_modules/(?!react-markdown|rehype-raw)"],
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.json",
+      useESM: true,
+    },
+  },
+  transformIgnorePatterns: ["/node_modules/(?!react-markdown|rehype-raw)"],
   coverageProvider: "v8",
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
